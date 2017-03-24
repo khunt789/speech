3a023bb75757d8107dee44bde340631a18f4a3a8
khunt789 <khunthardik90@gmail.com>
2017-03-24 17:47:23 +0530

    las

--- www/js/app.js
+++ www/js/app.js
@@ -1,47 +1,28 @@
-angular.module('starter', ['ionic'])
- 
-.controller('AppCtrl', function($scope) {
- 
-document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
+// Ionic Starter App
 
-window.TTS.speak({
-text: 'Welcome to the app! Welcome to the app! Welcome to the app! Welcome to the app!',
-locale: 'en-GB',
-rate: 0.75
-}, function () {
-alert('success');
-}, function (reason) {
-alert(reason);
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'app.directives'])
 
-});
-} 
- 
-  $scope.data = {
-    speechText: ''
-  };
-  $scope.recognizedText = '';
- 
-  $scope.speakText = function() {
-    TTS.speak({
-           text: $scope.data.speechText,
-           locale: 'en-GB',
-           rate: 1.5
-       }, function () {
-           // Do Something after success
-       }, function (reason) {
-           // Handle the error case
-       });
-  };
- 
-  $scope.record = function() {
-    var recognition = new SpeechRecognition();
-    recognition.onresult = function(event) {
-        if (event.results.length > 0) {
-            $scope.recognizedText = event.results[0][0].transcript;
-            $scope.$apply()
-        }
-    };
-    recognition.start();
-  };
-});
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .config(function ($ionicConfigProvider) {
+    $ionicConfigProvider.navBar.alignTitle('center');
+  });
